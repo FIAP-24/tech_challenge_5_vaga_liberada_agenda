4bc35975a4c605956cd9bcff23d34c301159634c
Renan da S. Silvaira <silveira.s.renan@gmail.com>
2026-01-18 17:58:17 -0300

    Commit inicial

---
+++ src/main/java/com/fiap/vaga_liberada_agenda/VagaLiberadaAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.vaga_liberada_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaLiberadaAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagaLiberadaAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/vaga_liberada_agenda/VagaLiberadaAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.vaga_liberada_agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaLiberadaAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
